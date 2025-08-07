cc683aea3d58f274744eb6eff588e749b85603e7
Ni Nguyen <nguyensuni1194@gmail.com>
2025-08-07 10:35:45 -0700

    Optimate Permution in String

--- src/main/kotlin/two_pointers/Permution in String.kt
+++ src/main/kotlin/two_pointers/Permution in String.kt
@@ -3,29 +3,60 @@ package two_pointers
 import kotlin.math.max
 
 /**
- * Given a string s, return true if it is a palindrome, otherwise return false.
- *
- * A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
- *
- * Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
  */
 fun main() {
-    val result = lengthOfLongestSubstring("aab")
+    val result = checkInclusion("ab", "eidboaoo")
     println(result)
 }
 
-fun lengthOfLongestSubstring(s: String): Int {
-    val mp = HashMap<Char, Int>()
-    var l = 0
-    var res = 0
+fun checkInclusion(s1: String, s2: String): Boolean {
+    if (s2.length < s1.length) return false
 
-    for (r in s.indices) {
-        if (s[r] in mp) {
-            l = maxOf(mp[s[r]]!! + 1, l)
-        }
-        mp[s[r]] = r
-        res = maxOf(res, r - l + 1)
+    val countSub = IntArray(26)
+    val countCheck = IntArray(26)
+
+    for (i in s1.indices) {
+        countSub[s1[i] - 'a']++
+        countCheck[s2[i] - 'a']++
     }
-    return res
+    var match = 0
 
+    for (i in 0 until 26) {
+        if (countSub[i] == countCheck[i]) match++
+    }
+
+    //window size at init 0 -> s1.length
+    var l = 0
+
+    //shift window to the right one by one
+    for (r in s1.length until s2.length) {
+        if (match == 26) return true
+
+        //increase window size
+        val rightCharIndex = s2[r] - 'a'
+        //update count
+        countCheck[rightCharIndex]++
+        if (countSub[rightCharIndex] == countCheck[rightCharIndex]) {
+            match++
+        } else if (countSub[rightCharIndex] + 1 == countCheck[rightCharIndex]) {
+            //decrease match when add another position
+            match--
+        }
+
+        //decrease window size
+        val leftCharIndex = s2[l] - 'a'
+        countCheck[leftCharIndex]--
+        if (countSub[leftCharIndex] == countCheck[leftCharIndex]) {
+            match++
+        } else if (countSub[leftCharIndex] - 1 == countCheck[leftCharIndex]) {
+            match--
+        }
+
+        l++
+
+    }
+    return match == 26
 }
