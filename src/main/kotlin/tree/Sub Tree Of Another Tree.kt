3fb9f9d2a3d0d0dfa96ce60428d8b583e14a6519
Ni Nguyen <nguyensuni1194@gmail.com>
2025-08-15 15:56:18 -0700

    Subtree of Another Tree

---
+++ src/main/kotlin/tree/Sub Tree Of Another Tree.kt
@@ -0,0 +1,22 @@
+package tree
+
+
+/**
+Subtree of Another Tree
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+ */
+
+fun isSubtree(root: TreeNode?, subRoot: TreeNode?): Boolean {
+    if (subRoot == null) return true
+    if (root == null) return false
+
+    if (isSameTree(root, subRoot)) return true
+
+    return isSubtree(root.right, subRoot) || isSubtree(root.left, subRoot)
+}
+
+fun isSameTree(root: TreeNode?, subRoot: TreeNode?): Boolean {
+    if (root == null && subRoot == null) return true
+    if (root == null || subRoot == null || root.`val` != subRoot.`val`) return false
+    return isSameTree(root.right, subRoot.right) && isSameTree(root.left, subRoot.left)
+}
