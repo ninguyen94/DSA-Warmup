256ec622fbc9502c94c0f4339df2fdd0f7995b17
Ni Nguyen <nguyensuni1194@gmail.com>
2025-08-15 12:01:53 -0700

    Invert Tree

---
+++ src/main/kotlin/tree/Invert Tree.kt
@@ -0,0 +1,23 @@
+package tree
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun invertTree(root: TreeNode?): TreeNode? {
+        if (root == null) return null
+
+        root.right = root.left.also { root.left = root.right }
+
+        invertTree(root.left)
+        invertTree(root.right)
+        return root
+    }
+}
